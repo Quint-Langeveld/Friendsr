2a5b6aed5549c370ee5d8022c4fa99bedf29ea24
Quint-Langeveld <quintlangeveld@hotmail.com>
2018-11-13 16:39:30 +0100

    update commit

---
+++ app/src/main/java/com/example/qlangeveld/friendsr/FriendsAdapter.java
@@ -0,0 +1,38 @@
+package com.example.qlangeveld.friendsr;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendsAdapter extends ArrayAdapter<Friend> {
+
+    private ArrayList<Friend> friends;
+
+    public FriendsAdapter(Context context, int resource, ArrayList<Friend> objects) {
+        super(context, resource, objects);
+        friends = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+        }
+
+        Friend current_friend = friends.get(position);
+
+        ((ImageView) convertView.findViewById(R.id.photo)).setImageResource(current_friend.getDrawableId());
+        ((TextView) convertView.findViewById(R.id.name)).setText(current_friend.getName());
+        return convertView;
+    }
+
+
+
+}
--- app/src/main/java/com/example/qlangeveld/friendsr/MainActivity.java
+++ app/src/main/java/com/example/qlangeveld/friendsr/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.qlangeveld.friendsr;
 
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
 
 import java.util.ArrayList;
 
@@ -17,25 +17,16 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<Friend> friends = new ArrayList<>();
 
-        Integer aryaImage = getResources().getIdentifier("arya", "photo", "arya.png");
-        Integer cerseiImage = getResources().getIdentifier("cercei", "photo", "cersei.png");
-        Integer daenerysImage = getResources().getIdentifier("daenerys", "photo", "daenerys.png");
-        Integer jaimeImage = getResources().getIdentifier("jaime", "photo", "jaime.png");
-        Integer jonImage = getResources().getIdentifier("jon", "photo", "joh.png");
-        Integer jorahImage = getResources().getIdentifier("joarh", "photo", "jorah.png");
-        Integer margaeryImage = getResources().getIdentifier("margaery", "photo", "margaery.png");
-        Integer sansaImage = getResources().getIdentifier("sansa", "photo", "sansa.png");
-        Integer tyrionImage = getResources().getIdentifier("tyrion", "photo", "tyrion.png");
-
-        Friend arya = new Friend("Arya", "A girl is Arya Stark of Winterfell, and I'm going home.", aryaImage);
-        Friend cersei = new Friend("Cersei", "When you play the game of thrones, you win or you die. There is no middle ground.", cerseiImage);
-        Friend daenerys = new Friend("Daenerys", "The world hadn't seen a dragon in centuries until my children were born.", daenerysImage);
-        Friend jaime = new Friend("Jaime", "There are no men like me. Only me.", jaimeImage);
-        Friend jon = new Friend("Jon", "And Jon's real name...", jonImage);
-        Friend jorah = new Friend("Jorah", "I wouldn't be here if I hadn't. I return to your srvice, my queen, if you'll have me", jorahImage);
-        Friend margaery = new Friend("Margaery", "I've had lots of time to think about how good I was at seeming good", margaeryImage);
-        Friend sansa = new Friend("Sansa", "I did what I had to do to survive, my lady. But I am a Stark, I will always be a Stark.", sansaImage);
-        Friend tyrion = new Friend("Tyrion", "I'm guilty of a far more monstrous crime: I'm guilty of being a dwarf!", tyrionImage);
+        Friend arya = new Friend("Arya", "A girl is Arya Stark of Winterfell, and I'm going home.", R.drawable.arya);
+        Friend cersei = new Friend("Cersei", "When you play the game of thrones, you win or you die. There is no middle ground.", R.drawable.cersei);
+        Friend daenerys = new Friend("Daenerys", "The world hadn't seen a dragon in centuries until my children were born.", R.drawable.daenerys);
+        Friend jaime = new Friend("Jaime", "There are no men like me. Only me.", R.drawable.jaime);
+        Friend jon = new Friend("Jon", "And Jon's real name...", R.drawable.jon);
+        Friend jorah = new Friend("Jorah", "I wouldn't be here if I hadn't. I return to your srvice, my queen, if you'll have me", R.drawable.jorah);
+        Friend margaery = new Friend("Margaery", "I've had lots of time to think about how good I was at seeming good", R.drawable.margaery);
+        Friend melisandre = new Friend("Melisandre", "The night is dark and full of terrors", R.drawable.melisandre);
+        Friend sansa = new Friend("Sansa", "I did what I had to do to survive, my lady. But I am a Stark, I will always be a Stark.", R.drawable.melisandre);
+        Friend tyrion = new Friend("Tyrion", "I'm guilty of a far more monstrous crime: I'm guilty of being a dwarf!", R.drawable.tyrion);
 
         friends.add(arya);
         friends.add(cersei);
@@ -44,15 +35,18 @@ public class MainActivity extends AppCompatActivity {
         friends.add(jon);
         friends.add(jorah);
         friends.add(margaery);
+        friends.add(melisandre);
         friends.add(sansa);
         friends.add(tyrion);
 
+        FriendsAdapter friendsAdapter = new FriendsAdapter(this, R.layout.grid_item, friends);
+
+        GridView gridView = findViewById(R.id.GridView);
+        gridView.setAdapter(friendsAdapter);
     }
 
-
-
-    public void goToSecond(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void goToProfile(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
 }
